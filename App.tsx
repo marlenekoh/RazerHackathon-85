3654312b7d3d615d42ad538b8b821b1af4fd82b3
marlenekoh <marlenekohxml@gmail.com>
2020-05-16 01:40:41 +0800

    Add react-navigation, module alias resolver

--- App.tsx
+++ App.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import Amplify from "aws-amplify";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import { HomeScreen } from "@screens/HomeScreen";
 import config from "./awsconfiguration.json";
 
+const Stack = createStackNavigator();
 Amplify.configure(config);
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const App: React.FunctionComponent = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
