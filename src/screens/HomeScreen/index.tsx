707f1238ccae726f269485474acc0732d154cce1
marlenekoh <marlenekohxml@gmail.com>
2020-05-16 02:18:53 +0800

    Add react-native-svg

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { SvgUri } from "react-native-svg";
 import { View, Text } from "react-native";
 
 export const HomeScreen: React.FunctionComponent = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <SvgUri
+        width="250"
+        height="250"
+        uri="http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
+        color="black"
+      />
       <Text>Home Screen</Text>
     </View>
   );
